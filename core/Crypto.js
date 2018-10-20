d179d11e7d1d0725e35e02727ab60a36945273ae
amirsaleem-kn <amir.saleem@kisannetwork.com>
2018-10-20 23:37:40 +0530

    add comments

--- core/Crypto.js
+++ core/Crypto.js
@@ -2,7 +2,12 @@ const crypto = require('crypto');
 const server_key = require('../keys/server_key');
 const algorithm = 'aes-256-ctr';
 
+/**
+ * @description top level class to use cryptographic functions
+ */
+
 class Crypto {
+    // encrypt a string
     static encrypt (str) {
         var cipher = crypto.createCipher(algorithm, server_key);
         var crypted = cipher.update(str,'utf8','hex');
@@ -10,22 +15,31 @@ class Crypto {
         return crypted;
     } 
 
+    // decrypt a string
     static decrypt (str) {
         var decipher = crypto.createDecipher(algorithm, server_key)
         var dec = decipher.update(str,'hex','utf8')
         dec += decipher.final('utf8');
         return dec;
     }
+
+    // create a salt
     static createSalt () {
         return crypto.randomBytes(16).toString('hex');
     }
+
+    // create a hash using string and a salt
     static createHash (str, salt) {
         return crypto.pbkdf2Sync(str, salt, 10000, 512, 'sha512').toString('hex');
     }
+
+    // match the hash values
     static validateHash (str, salt, hashValue) {
         const hash = this.createHash(str, salt);
         return hash == hashValue;
     }
+
+    // generate an md5 token
     static generateToken (str) {
         return crypto.createHash('md5').update(str).digest('hex');
     }
--- public/routes/sign_in.js
+++ public/routes/sign_in.js
@@ -4,36 +4,52 @@ const Crypto = require('../../core/Crypto');
 const { base64_decode } = require('../../lib/common/helper_functions');
 const { Debugger, Logger } = require('../../etc/logs/logger');
 
+/**
+ * @description method to sign-in the user and generate an authentication token
+ * @param {type:obj} req an HTTP request object
+ * @param {type:res} res an HTTP response object
+ */
+
 function signIn(req, res) {
     const credentials = req.get('Authorization') || null;
+    // check if credentials are not present
     if(!credentials) {
-        Response.forbidden(res);
+        Response.forbidden(res); // send 403
         return;
     };
-    Debugger.log('here');
+    // split the string on space
     var splittedCreds = credentials.split(' ');
+    // if credentials first part is not basic, send 403
     if(splittedCreds[0] != 'Basic'){
         Response.forbidden(res);
         return;
     }
+    // get the username and password out from the string
     const base64creds = splittedCreds[1];
     const decodedCreds = base64_decode(base64creds).split(':');
     const username = decodedCreds[0];
     const password = decodedCreds[1];
-    // check if user exists
+    
+    // get an active mysql connection
     db.getConnection(function(err, connection){
         if(err) {
             Response.serviceError(res);
             return;
         }
+        // get user details using username
         getUser(connection, username).then(userData => {
+            // if user does not found, send 403
             if(userData.length == 0) {
                 Response.forbidden(res);
                 return;
             }
+            // compare hash of the user's password
             const validUser = Crypto.validateHash(password, userData[0]['salt'], userData[0]['hash']);
+            // if hash matches
             if(validUser) {
+                // generate a new token
                 const token = Crypto.generateToken(req.get('User-Agent'));
+                // store token in database
                 storeToken(connection, userData[0]['userID'], token).then(data => {
                     connection.release();
                     Response.success(res, {
@@ -53,6 +69,13 @@ function signIn(req, res) {
     });
 }
 
+/**
+ * @description method to store token in AccessToken table against a userID
+ * @param {type:object} connection an active mysql connection
+ * @param {type:string} userID id of the user
+ * @param {type:hash} token a token hash 
+ */
+
 function storeToken (connection, userID, token) {
     Debugger.log('HERE HERE');
     return new Promise((resolve, reject) => {
@@ -71,6 +94,12 @@ function storeToken (connection, userID, token) {
         });
     });   
 }
+
+/**
+ * @description method to get user details from username
+ * @param {type:object} connection an active mysql connection
+ * @param {type:string} username username of the user 
+ */
 
 function getUser (connection, username) {
     return new Promise ((resolve, reject) => {
