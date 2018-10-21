5a6861d4dc566dbe59c3f851af0c278a8e897211
amirsaleem-kn <amir.saleem@kisannetwork.com>
2018-10-21 09:46:30 +0530

    add authenticatio logic in middleware

--- middlewares/request_middlewares.js
+++ middlewares/request_middlewares.js
@@ -1,7 +1,14 @@
+/**
+ * @author Amir Saleem
+ * @description this file contains the application middlewares
+ */
+
 const app = require('../core/App');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const { Logger, Debugger } = require('../etc/logs/logger');
+const db = require('../core/Database');
+const Response = require('../etc/response_template');
 
 /**
  * @description external middlewares
@@ -40,7 +47,55 @@ app.use(function(req, res, next){
  * @description authentication middleware
  */
 
+const _excludeAuthenticationForPaths = ['/sign-in'];
+
 app.use(function(req, res, next){
   Debugger.log('Verifying Authenticity');
-  next();  
+  if(_excludeAuthenticationForPaths.indexOf(req.path) > -1) {
+      // we do not need token if user is signing in
+      return next();
+  }
+  // now we need token to authenticate the incoming requests
+  authenticateRequest(req, function(authenticated){
+      if(authenticated) {
+        return next();
+      }
+      Response.notAuthorized(res);
+      return;
+  });  
 });
+
+function authenticateRequest (req, callback) {
+    Debugger.fancy('Authenticating the request');
+    var credential = req.get('Authorization') || null;
+    var userID = req.get('userID') || null;
+    if(!credential || !userID){
+        callback(false);
+        return;
+    }
+    var splittedCred = credential.split(' ');
+    if(splittedCred[0] != 'Bearer'){
+       callback(false);
+       return;
+    }  
+    const userToken = splittedCred[1] || null;
+    db.executeQuery({
+        query: "SELECT token from AccessToken where userID = ?",
+        queryArray: [userID],
+        connection: connection
+    }, function(err, result){
+        if(err) {
+            callback(false);
+            return;
+        }
+        if(result.length == 0){
+            callback(false);
+            return;
+        } else if (result[0]['token'] == userToken) {
+            callback(true);
+        } else {
+            callback(false);
+        }      
+        return;
+    });
+}
