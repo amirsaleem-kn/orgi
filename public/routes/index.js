1ad42c1ea56e9ce6e1729ad3785ef234b16a497a
amirsaleem-kn <amir.saleem@kisannetwork.com>
2018-10-21 18:03:18 +0530

    add logout route

--- public/routes/index.js
+++ public/routes/index.js
@@ -1,8 +1,10 @@
 const app = require('../../core/App');
 const Response = require('../../etc/response_template');
 const signIn = require('./sign_in');
+const logout = require('./logout');
 
 app.get('/', (req, res) => {
+    console.log(res.locals);
     Response.success(res, {
         system: '5 GB',
         cpu: '78%',
@@ -16,3 +18,4 @@ app.get('/', (req, res) => {
 });
 
 app.post('/sign-in', signIn);
+app.post('/logout', logout);
