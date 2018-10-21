55fc55350152d6575b30ad7486e7b83385098b1e
amirsaleem-kn <amir.saleem@kisannetwork.com>
2018-10-21 18:03:32 +0530

    add jwt token logic

---
+++ public/web_token.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const keys = require('../keys/keys');
+const server_key = require('../keys/server_key');
+
+class JWT {
+    static generateJWT(userId, username) {
+        const authExpiry = Date.now() + keys.authExpiry;
+        return jwt.sign({
+            id: userId,
+            username: username,
+            userId: userId,
+            exp: Math.floor(Date.now() / 1000) + 60 * 60
+        }, server_key);
+    }
+
+    static toAuthJSON(userId, username) {
+        return {
+            username: username,
+            token: this.generateJWT(userId, username),
+        };
+    }
+
+    static verifyJWT (token, callback) {
+        jwt.verify(token, server_key, function(err, message){
+            callback(err, message);
+        });
+    }
+}
+
+module.exports = JWT;
